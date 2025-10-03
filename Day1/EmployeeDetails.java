62ab7215b8f5e512bebc96eef80ce6fffea6aaa9
vijaibaskar44 <vijaibaskar44@gmail.com>
2025-10-03 22:34:56 +0530

    Add files via upload
    day1

---
+++ Day1/EmployeeDetails.java
@@ -0,0 +1,31 @@
+package week1.Day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		  String name = "John Doe";
+				int  age = 30;
+				 float salary = 55000.75f;
+				 char gender = 'M';
+				 boolean acceptTerms = true;
+				 float rating = 4.5f;
+				 long mobileNumber = 9876543210l;
+				 long pinCode = 56001l;
+				 int luckyNumber = 7;
+				 
+				 System.out.println("Employee Name is" +name);
+
+				 System.out.println("Employee age is" +age);
+				 System.out.println("Employee salary is" +salary);
+				 System.out.println("Employee gender is" +gender);
+				 System.out.println("Employee acceptTerms is" +acceptTerms);
+				 System.out.println("Employee rating is" +rating);
+				 System.out.println("Employee mobileNumber is" +mobileNumber);
+				 System.out.println("Employee pinCode is" +pinCode);
+				 System.out.println("Employee luckyNumber is" +luckyNumber);
+				 
+				 
+				 
+	}
+
+}
