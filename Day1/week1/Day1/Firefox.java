ea1431153b9a6b58528088dce7bc5903849bf58d
vijaibaskar44 <vijaibaskar44@gmail.com>
2025-10-05 16:56:49 +0530

    Add files via upload
    week1

---
+++ Day1/week1/Day1/Firefox.java
@@ -0,0 +1,23 @@
+package week1.Day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+float browserVersion=  100.2f;
+String browserName = "firefox";
+boolean isVisible = true;
+int releaseYear= 1998;
+char browserLogo = 'f';
+
+System.out.println(browserVersion);
+System.out.println(browserName);
+System.out.println(isVisible);
+System.out.println(releaseYear);
+System.out.println(browserLogo);
+
+
+
+
+	}
+
+}
---
+++ Day1/week1/Day1/HelloTestLeaf.java
@@ -0,0 +1,10 @@
+package week1.Day1;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+	System.out.println("welcome to testleaf");
+
+	}
+
+}
